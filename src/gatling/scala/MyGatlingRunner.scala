1c9b17a2c37e5eee72efd9cb103b91f0dddf3fe2
Vitalii Marchenko <vitalymarchenko@Vitalys-MacBook-Pro.local>
2020-11-11 16:29:24 +0200

    Add example with csv feeder Add example with code generated primitive feeder

--- src/gatling/scala/MyGatlingRunner.scala
+++ src/gatling/scala/MyGatlingRunner.scala
@@ -1,12 +1,12 @@
 import io.gatling.app.Gatling
 import io.gatling.core.config.GatlingPropertiesBuilder
-import simulations.{AddPauseTime, CheckResponseBodyAndExtract, CheckResponseCode, CodeReuseWithObjects}
+import simulations.{AddPauseTime, CheckResponseBodyAndExtract, CheckResponseCode, CodeReuseWithObjects, CsvFeeder, CsvFeederToCustom}
 
 object MyGatlingRunner {
 
   def main(args: Array[String]): Unit = {
 
-    val simClass = classOf[CodeReuseWithObjects].getName
+    val simClass = classOf[CsvFeederToCustom].getName
 
     val props = new GatlingPropertiesBuilder
     props.simulationClass(simClass)
